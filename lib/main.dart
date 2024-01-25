32bacda8e8dcda8227efbfbaadd2ce2dac0d7831
Amin Faruq <aminfaruk.fa@gmail.com>
2024-01-25 23:35:07 +0800

    adjust main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:list_product_json/data/product.dart';
+import 'package:list_product_json/data/api/api_service.dart';
 import 'package:list_product_json/detail/product_detail.dart';
 import 'package:list_product_json/home/list_product.dart';
+import 'package:list_product_json/providers/get_products_providers.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,23 +14,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(useMaterial3: true),
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/home',
-      routes: {
-        '/home': (context) => const ListProduct(),
-        '/detail': (context) {
-          final arguments = ModalRoute.of(context)?.settings.arguments;
-          if (arguments is Product) {
-            return ProductDetail(product: arguments);
-          } else {
-            // Handle the case when arguments are not of type Product
-            // You can navigate to an error page or handle it in another way
-            return Container();
-          }
-        },
-      },
-    );
+    return ChangeNotifierProvider(
+        create: (_) => GetProductsProvider(apiService: ApiService()),
+        child: MaterialApp(
+          theme: ThemeData(useMaterial3: true),
+          debugShowCheckedModeBanner: false,
+          initialRoute: '/home',
+          routes: {
+            '/home': (context) => const ListProduct(),
+            '/detail': (context) {
+              final arguments = ModalRoute.of(context)?.settings.arguments;
+              if (arguments is String) {
+                return ProductDetail(id: arguments);
+              } else {
+                // Handle the case when arguments are not of type Product
+                // You can navigate to an error page or handle it in another way
+                return Scaffold(appBar: AppBar(), body: Container());
+              }
+            },
+          },
+        ));
   }
 }
