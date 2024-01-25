0b6c3d0bbd478d0c737fc2f932ef3e90ee58170e
Amin Faruq <aminfaruk.fa@gmail.com>
2024-01-25 21:42:37 +0800

    adjust model

--- lib/data/product.dart
+++ lib/data/product.dart
@@ -1,51 +1,103 @@
-class Product {
-  final String title;
-  final String description;
-  final int price;
-  final num rating;
-  final int stock;
-  final String brand;
-  final String category;
-  final String thumbnail;
-  final List<String> images;
+import 'dart:convert';
 
-  Product({
-    required this.title,
-    required this.description,
-    required this.price,
-    required this.rating,
-    required this.stock,
-    required this.brand,
-    required this.category,
-    required this.thumbnail,
-    required this.images,
+class BaseProductResponse {
+  final List<Product>? products;
+  final int? total;
+  final int? skip;
+  final int? limit;
+
+  BaseProductResponse({
+    this.products,
+    this.total,
+    this.skip,
+    this.limit,
   });
 
-  factory Product.fromJson(Map<String, dynamic> json) {
-    return Product(
-      title: json['title'],
-      description: json['description'],
-      price: json['price'],
-      rating: json['rating'],
-      stock: json['stock'],
-      brand: json['brand'],
-      category: json['category'],
-      thumbnail: json['thumbnail'],
-      images: List<String>.from(json['images']),
-    );
-  }
+  factory BaseProductResponse.fromRawJson(String str) =>
+      BaseProductResponse.fromJson(json.decode(str));
 
-  Map<String, dynamic> toJson() {
-    return {
-      'title': title,
-      'description': description,
-      'price': price,
-      'rating': rating,
-      'stock': stock,
-      'brand': brand,
-      'category': category,
-      'thumbnail': thumbnail,
-      'images': images,
-    };
-  }
+  String toRawJson() => json.encode(toJson());
+
+  factory BaseProductResponse.fromJson(Map<String, dynamic> json) =>
+      BaseProductResponse(
+        products: json["products"] == null
+            ? []
+            : List<Product>.from(
+                json["products"]!.map((x) => Product.fromJson(x))),
+        total: json["total"],
+        skip: json["skip"],
+        limit: json["limit"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "products": products == null
+            ? []
+            : List<dynamic>.from(products!.map((x) => x.toJson())),
+        "total": total,
+        "skip": skip,
+        "limit": limit,
+      };
+}
+
+class Product {
+  final int? id;
+  final String? title;
+  final String? description;
+  final int? price;
+  final double? discountPercentage;
+  final double? rating;
+  final int? stock;
+  final String? brand;
+  final String? category;
+  final String? thumbnail;
+  final List<String>? images;
+
+  Product({
+    this.id,
+    this.title,
+    this.description,
+    this.price,
+    this.discountPercentage,
+    this.rating,
+    this.stock,
+    this.brand,
+    this.category,
+    this.thumbnail,
+    this.images,
+  });
+
+  factory Product.fromRawJson(String str) => Product.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory Product.fromJson(Map<String, dynamic> json) => Product(
+        id: json["id"],
+        title: json["title"],
+        description: json["description"],
+        price: json["price"],
+        discountPercentage: json["discountPercentage"]?.toDouble(),
+        rating: json["rating"]?.toDouble(),
+        stock: json["stock"],
+        brand: json["brand"],
+        category: json["category"],
+        thumbnail: json["thumbnail"],
+        images: json["images"] == null
+            ? []
+            : List<String>.from(json["images"]!.map((x) => x)),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "title": title,
+        "description": description,
+        "price": price,
+        "discountPercentage": discountPercentage,
+        "rating": rating,
+        "stock": stock,
+        "brand": brand,
+        "category": category,
+        "thumbnail": thumbnail,
+        "images":
+            images == null ? [] : List<dynamic>.from(images!.map((x) => x)),
+      };
 }
